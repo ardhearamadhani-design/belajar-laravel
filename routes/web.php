d31b37ef8d30ee24064c71cadf85f957b56ed9e4
Ardhea Regita Ramadhani <ardhearamadhani@gmail.com>
2026-08-18 15:26:02 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/halo', function () {
+    return ('Selamat datang');
+});
